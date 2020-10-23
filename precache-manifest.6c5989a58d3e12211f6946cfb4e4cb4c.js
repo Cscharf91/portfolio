af4c4f10ec00d3be0dae142bb151296c21c06b6e
Cscharf91 <cscharf91@gmail.com>
2020-10-23 14:17:00 -0400

    Updates

---
+++ precache-manifest.6c5989a58d3e12211f6946cfb4e4cb4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1c96a4e4d303d55da047d88afbcad4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9be35ef76caae892e5ce",
+    "url": "/portfolio/static/css/main.0a3f9518.chunk.css"
+  },
+  {
+    "revision": "334d6208a25d0b3e2727",
+    "url": "/portfolio/static/js/2.80811c8e.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/portfolio/static/js/2.80811c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be35ef76caae892e5ce",
+    "url": "/portfolio/static/js/main.ed19f502.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "24747c66f566564d01f6cbe3f7dcee9a",
+    "url": "/portfolio/static/media/battleship.24747c66.png"
+  },
+  {
+    "revision": "6d834868c55fbed3d39905401d66c7e6",
+    "url": "/portfolio/static/media/chess.6d834868.png"
+  },
+  {
+    "revision": "91eac2770714cf4842b445c912820234",
+    "url": "/portfolio/static/media/headshot.91eac277.jpg"
+  },
+  {
+    "revision": "39e8c2f968ff0bb094da94ae94f0c6aa",
+    "url": "/portfolio/static/media/rjs-billing.39e8c2f9.png"
+  }
+]);
